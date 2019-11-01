b648928dbd6081330b145dcd7c92690795eefb17
AlanTeoYueYang <47876194+AlanTeoYueYang@users.noreply.github.com>
2019-11-01 11:36:14 +0800

    Update PPI_Network.py

--- PPI_Network.py
+++ PPI_Network.py
@@ -1,22 +1,26 @@
 import numpy as np
-import Protein
 
 class PPI_Network:
 	def __init__(self, protein_links, protein_aliases): 
 		# download protein.links.full.v__.txt and protein.aliases.v__.txt from https://string-db.org/cgi/download.pl
-		self.network = PPI_Network.create_network(self, protein_links, protein_aliases)
-		self.matrix = PPI_Network.create_matrix(self)
-		self.RWR_matrix = PPI_Network.creating_RWR_matrix(self)
+		self.matrix = np.zeros((1,1))
+		self.indices = {}
+		self.names = []
+		self.add_network(protein_links, protein_aliases)
 
 	def protein_id(id_string):
 		return id_string
 
-	def create_network(self, protein_links, protein_aliases):
+	def add_protein_to_matrix(self):
+		col = np.zeros((len(self.matrix), 1))
+		self.matrix = np.concatenate((self.matrix, col), 1)
+		row = np.zeros((1, len(self.matrix[0])))
+		self.matrix = np.concatenate((self.matrix, row), 0)
+
+	def add_network(self, protein_links, protein_aliases):
 		print("Creating network")
-		protein_network = {}
-		protein_indices = []
-		data = open(protein_links)
-		names = open(protein_aliases)
+		data = open(protein_links, 'r')
+		names = open(protein_aliases, 'r')
 		index = 0
 		headers = True
 		for i in data:
@@ -26,48 +30,36 @@ class PPI_Network:
 				for j in temp [3:15]: # setting channels to channel of interest; can be altered accordingly
 					score *= (1-int(j)/1000) # calculate confidence scores of links
 				score = 1 - score		
-				protein_a_id = PPI_Network.protein_id(temp[0]) 
-				protein_b_id = PPI_Network.protein_id(temp[1])
-				if protein_a_id not in protein_network:
-					protein_network[protein_a_id] = Protein(protein_a_id, index)
-					protein_indices.append(protein_a_id)
+				protein_a_id = temp[0] 
+				protein_b_id = temp[1]
+				if protein_a_id not in self.indices:
+					self.indices[protein_a_id] = index
+					protein_a_index = index
+					if index > 0: self.add_protein_to_matrix()
 					index+=1
-				if protein_b_id not in protein_network:
-					protein_network[protein_b_id] = Protein(protein_b_id, index)
-					protein_indices.append(protein_b_id)
-					index+=1
+					self.names.append([])
+				if protein_b_id not in self.indices:
+					self.indices[protein_b_id] = index
+					protein_b_index = index
+					self.add_protein_to_matrix()
+					index += 1
+					self.names.append([])
 				if score >= 0.4:
-					protein_network[protein_a_id].add_neighbour(protein_b_id,score)
-					protein_network[protein_b_id].add_neighbour(protein_a_id,score)
+					self.matrix[protein_a_index][protein_b_index] = score
 			else:
 				headers = False
-		self.indices = protein_indices
 		print("Adding names")
 		headers = True
 		for k in names:
 			if headers == False:
 				temp = k.split("\t")
-				k_id = PPI_Network.protein_id(temp[0])
 				curr_name = temp[1]
-				if k_id in protein_network:
+				if temp[0] in self.indices:
 					# annotating names to proteins, criteria can be set accordingly
-					if "HUMAN" in curr_name and type(protein_network[k_id].name) == int:
-						protein_network[k_id].name = curr_name
+					idx = self.indices[temp[0]]
+					if "HUMAN" in curr_name:
+						self.names[idx] = curr_name
 					if temp[2].split(" ")[0] == "BLAST_UniProt_ID":
-						protein_network[k_id].name = curr_name
+						self.names[idx] = curr_name
 			else:
 				headers = False
-		return np.array(protein_network)
-
-	def create_matrix(self):
-		print("Creating matrix")
-		matrix = np.zeros(shape=(len(self.network),len(self.network)))
-		for p_id in self.network:
-			p = self.network[p_id]
-			i = p.index  
-			for neighbour_id in p.neighbours:
-				neighbour = self.network[neighbour_id]
-				score = p.neighbours[neighbour_id]
-				j = neighbour.index
-				matrix[i,j] = score
-		return matrix
