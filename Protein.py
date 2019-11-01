ef5bacfd935cbd9168a2e145a8e6420ef83149a2
AlanTeoYueYang <47876194+AlanTeoYueYang@users.noreply.github.com>
2019-11-01 11:35:10 +0800

    Delete Protein.py

--- Protein.py
+++
@@ -1,9 +0,0 @@
-class Protein:
-	def __init__(self, id, index):
-		self.id = id
-		self.index = index
-		self.neighbours = {}
-		self.name = id
-
-	def add_neighbour(self, neighbour, score):
-		self.neighbours[neighbour] = score
