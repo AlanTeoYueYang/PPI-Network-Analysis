581cf46fc387673f5469da6997347b8ad2278ba1
AlanTeoYueYang <47876194+AlanTeoYueYang@users.noreply.github.com>
2019-11-01 11:39:56 +0800

    Update Propagation_analysis.py

--- Propagation_analysis.py
+++ Propagation_analysis.py
@@ -1,9 +1,9 @@
-import PPI_Network
+from PPI_Network import PPI_Network
 
 class Propagation_analysis(PPI_Network):
 	def __init__(self, protein_links, protein_aliases):
 		# download protein.links.full.v__.txt and protein.aliases.v__.txt from https://string-db.org/cgi/download.pl
-		super(Propagation_analysis, self).__init__(protein_links, protein_aliases)
+		super().__init__(protein_links, protein_aliases)
 
 	def creating_flow_matrix(self):
 		print("Creating propogation matrix")
@@ -16,12 +16,11 @@ class Propagation_analysis(PPI_Network):
 					temp_matrix[r][c] /= norm
 		return temp_matrix	
 
-	def propogation(self, candidate_genes, alpha, flow_matrix):
+	def propogation(self, candidate_genes_id, alpha, flow_matrix):
 		print("Performing propogation")
-		candidate_genes_ids = list(map(lambda x: PPI_Network.protein_id(x), candidate_genes))
 		Y0 = np.zeros(len(self.network))
 		for gene_id in candidate_genes_ids:
-			Y0[self.network[gene_id].index] = 1
+			Y0[self.indices[gene_id]] = 1
 		L2_norm = 1
 		iteration = 0
 		Y_next = Y0.copy()
@@ -32,33 +31,23 @@ class Propagation_analysis(PPI_Network):
 			iteration += 1
 			if (iteration > 1000000):
 				break
-		d = {}
-		lst = list(Y_next)
-		for i in range(len(lst)):
-			if lst[i] not in d:
-				d[lst[i]] = []
-			d[lst[i]].append(self.indices[i])
-		sorted_d = collections.OrderedDict(sorted(d.items(), reverse = True))
-		rank = []
-		for m in sorted_d:
-			for n in sorted_d[m]:
-				if n not in candidate_genes_ids:
-					rank.append(n)
+		for i, v in enumerate(Y_next):
+			Y_next[i] = [self.names[i],v]
+		rank = sorted(Y_next, key=lambda x: x[1], reverse=True)
+		return rank
+
 		return rank
 
 	def intersection(self, disease1_genes, disease2_genes, alpha):
 		flow_matrix = PPI_Network.creating_flow_matrix(self)
-		rank1 = PPI_Network.propogation(self,disease1_genes, alpha, flow_matrix)
-		rank2 = PPI_Network.propogation(self,disease2_genes, alpha, flow_matrix)
+		ranks = {}
 		print("Finding intersection")
-		d = {}
-		for i in range(len(rank1)):
-			d[rank1[i]] = i
-		for j in range(len(rank2)):
-			if rank2[j] in d:
-				d[rank2[j]] += j
-		sorted_d = collections.OrderedDict(sorted(d.items(), key=lambda x: x[1]))
-		intersectn = []
-		for k in sorted_d:
-			intersectn.append([test.network[k].name, sorted_d[k]])
-		return intersectn
+		for dis_genes in diseases_genes:
+			rank = self.propogation(dis_genes, alpha)
+			for gene, rank_no in rank:
+				if gene not in ranks:
+					ranks[gene] = rank_no
+				else:
+					ranks[gene] += rank_no
+		sorted_ranks = collections.OrderedDict(sorted(d.items(), key=lambda x:x[1]))
+		return sorted_ranks
